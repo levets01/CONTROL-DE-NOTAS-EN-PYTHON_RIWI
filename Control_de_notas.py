5a8a1a82272f72039d92daf1d79e0a7ea2194c61
ERROR_404 <68274153+levets01@users.noreply.github.com>
2026-03-11 09:01:05 -0500

    Fix validation and output issues in Control_de_notas.py

--- Control_de_notas.py
+++ Control_de_notas.py
@@ -4,6 +4,13 @@ print("░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░▀�
 
 #aca esta la lista en la que se almacenan los estudiantes
 estudiantes = []
+#aca estan las variasbles donde se van a almacenar los datos de los couder 
+suma_notas = 0
+reprobados = 0
+regulares = 0
+execelentes = 0
+mejor_nota = 0
+mejor_couder = ""
 
 modulos = input("\n INGRESE EL NOMBRE DEL MODULO: ")
 cantidad = int(input("INGRESE LA CANTIDAD DE COUDER: "))
@@ -13,17 +20,17 @@ cantidad = int(input("INGRESE LA CANTIDAD DE COUDER: "))
 
 def validar_nota(mensaje):
     while True:
-         nota = float(input(mensaje))
-         if 0 <= nota <= 100 :
-              return nota 
-         else:
-              print("ERROR LA NOTA DEBE ESTAR ENTRE 0 Y 100 ")
+        nota = float(input(mensaje))
+if 0 <= nota <= 100 :
+            return nota
+        else:
+            print("ERROR LA NOTA DEBE ESTAR ENTRE 0 Y 100 ")
 
 for i in range (cantidad):
     print("\n COUDER", i+1)
 
     nombre = input ("INGRESE EL NOMBRE DEL COUDER : ")
-   
+
 
     desarrollo = validar_nota ("INGRESE LA NOTA DE DESARROLLO")
     ingles = validar_nota ("INGRESE LA NOTA DE INGLES: ")
@@ -32,7 +39,7 @@ for i in range (cantidad):
     if input == "*" :
         break
 
-         
+
 
 
     nota_final = (desarrollo * 0.6 ) + (ingles * 0.2) + (habilidades *0.2)
@@ -44,12 +51,12 @@ for i in range (cantidad):
         reprobados += 1
         
     elif nota_final < 80 :
-        clasificacion = "Regular" 
+        clasificacion = "Regular"
         regulares += 1
         
     else:
         clasificacion = "Excelente"
-        excelentes  += 1   
+        excelentes  += 1
         
         if nota_final > mejor_nota:
             mejor_nota = nota_final
@@ -64,7 +71,7 @@ for i in range (cantidad):
                 "nota_final" : nota_final,
                 "clasificacion" : clasificacion
             }
-    estudiantes.append(estudiante) 
+    estudiantes.append(estudiante)
     
     
 print ("\n RESULTADOS")
@@ -79,7 +86,7 @@ for est in estudiantes:
     
     print("\n RESUMEN DEL MODULO")
     print("Modulo:", modulos)
-    print("coders resguistrados:", les(estudiantes))
+    print("coders resguistrados:", len(estudiantes))
     print("Promedio general:" , round(promedio_grupo,2))
     
     print("\n CLASIFICACION DEL GRUPO")
